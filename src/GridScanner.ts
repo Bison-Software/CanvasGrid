da56c71774a22a7540ec51992d19ac24af67c6b2
Fonzi <me@fonzi.xyz>
2025-10-06 09:43:19 -0500

    Adds more licensing comments

--- src/GridScanner.ts
+++ src/GridScanner.ts
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2025 Bison Software LLC
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import type { Page } from '@playwright/test';
 import type { Grid } from './GridOverlay.js';
 import type { MouseInteractions } from './MouseInteractions.js';
